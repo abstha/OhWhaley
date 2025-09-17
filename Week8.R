71ef1af4d58d5b55ab9897daa29faf2f33a20b47
Abhinav Shrestha <abhinavshrestha03@gmail.com>
2025-09-17 15:59:03 +1000

    Created package skeleton

--- Week8.R
+++ Week8.R
@@ -1,2 +1,3 @@
-install.packages(c("devtools", "roxygen2", "testthat", 
-                   "knitr", "usethis", "pkgdown"))
+library(devtools)
+# Choose your path carefully!
+usethis::create_package("~/Documents/Github/OhWhaley")
