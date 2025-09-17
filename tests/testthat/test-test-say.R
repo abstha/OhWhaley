9a8a27d6515c53b8d057361497885d5a8f7ec459
Abhinav Shrestha <abhinavshrestha03@gmail.com>
2025-09-17 16:51:00 +1000

    Added Unit tests

---
+++ tests/testthat/test-test-say.R
@@ -0,0 +1,6 @@
+test_that("Output is of correct format",{
+  expect_length(say(), 0) #Returns something with a length of 0
+  expect_null(say()) #Returns null
+  expect_invisible(say()) #Returns invisibly
+  expect_message(say()) #Returns a message
+})
